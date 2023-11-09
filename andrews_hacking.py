ee3f390d70fbca9f3985889f8d21156b608ec108
Andrew Stribling <andrewstribling@Andrews-MBP.lan>
2023-11-09 15:28:56 -0600

    updated functions to include correct parameters inside of the main function.

--- andrews_hacking.py
+++ andrews_hacking.py
@@ -5,28 +5,32 @@ name = "Python"
 
 # garbage characters system
 garbage_chars = ["~!@#$%^&*()_+-={}[]|;:,.<>?/"]
+num_of_matches = 3
+count = 0
 
 
 def main():
     introduce()
     word_list = get_word_list()
     password = get_password(word_list)
-    get_matching_letter_words(password, word_list, num_matches, count)
-    game_word_list = combine_word_lists()
+    get_matching_letter_words(password, word_list, num_matches=3, count=3)
+    game_word_list = combine_word_lists(password, word_list)
+    print(game_word_list)
 
 
 # introduce game
 def introduce():
     print(
-        f"Agent {name}! The evil dictator Kim Jong Un has decided to launch the nukes. All of humanites hopes rest on your shoulders to hack into the system and stop the launch. You will see a series of possible words that are the password. Use our hint system software to determine if you are closs to guessing the password. You are our last hope.... \n Okay agent {name}, here are list of the possible passwords. \n Type in the word from the available list of words that you believe is the password."
+        f"Agent {name}! The evil dictator Kim Jong Un has decided to launch the nukes. All of humanity's hopes rest on your shoulders to hack into the system and stop the launch. You will see a series of possible words that are the password. Use our hint system software to determine if you are closs to guessing the password. You are our last hope.... \n Okay agent {name}, here are list of the possible passwords. \n Type in the word from the available list of words that you believe is the password."
     )
+
 
 # from the text file pull out all of the game words.
 def get_word_list():
     with open("sevenletterwords.txt", "r") as file:
         word_list = [line.strip().upper() for line in file.readlines()]
         # test print(full_list_of_words)
-        return word_list
+    return word_list
 
 
 # This function generates a password for the player to guess.
@@ -51,7 +55,9 @@ def combine_word_lists(password, word_list):
     one_letter_words = get_matching_letter_words(password, word_list, 1, 3)
     three_letter_words = get_matching_letter_words(password, word_list, 3, 3)
     five_letter_words = get_matching_letter_words(password, word_list, 3, 5)
-    game_list = merge(zero_letter_words, one_letter_words, three_letter_words, five_letter_words)
+    game_list = merge(
+        zero_letter_words, one_letter_words, three_letter_words, five_letter_words
+    )
     return game_list
 
 
