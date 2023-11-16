a98b676ea9b07192a7adc253374a5f8b215f4cb9
Andrew Stribling <andrewstribling@Andrews-MBP.lan>
2023-11-15 23:26:25 -0600

    get_game_words function

---
+++ others.py
@@ -0,0 +1,5 @@
+# @mtf
+# url https://discuss.codecademy.com/t/combing-function-values-into-one-list/789693/40
+
+# I would like to thank mtf for pointing out generators to me. 
+# ok are you able to run this and debug it ?
--- outline.md
+++
@@ -1,5 +0,0 @@
-determine the correct word
-
-function looking for the first 3 letters. 
-
-function looking for the 
