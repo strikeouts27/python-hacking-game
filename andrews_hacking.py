eb1b397a309df6cebef0e14fc918e5979c6d7cbb
Andrew Stribling <andrewstribling@Andrews-MacBook-Pro.local>
2023-12-12 16:39:33 -0600

    established the game words

--- andrews_hacking.py
+++ andrews_hacking.py
@@ -2,6 +2,16 @@
 import sys
 import random
 
+"""
+https://inventwithpython.com/bigbookpython/project33.html
+basically game_words is where I am telling python to grab the 15 words used fo the hacking mini game.
+
+Players select from this list of words hoping to guess the password.
+
+I need 3 words that have 0 letters in common with the password
+and another 3 words that have 1,2,3,4 letters in common with the password respectivley and the password all stored in a way that python can know about it.
+after that I need to make a game board that will display the characters and the words on the screen.
+"""
 # -----------------------------------------------------------------#
 
 # CONSTANTS
@@ -38,40 +48,63 @@ def get_word_list():
 # This function generates a password for the player to guess.
 def get_password(word_list):
     password = random.choice(word_list)
-    # print("test: This is a test for the correct word: " + correct_word)
+    print("test: This is a test for the password: ", password)
     return password
 
 
 # my goal is to use this function to get 3 0 letter matching words and 3 1 letter matching words and 3 2 letter matching words and so on.
 # a set of game words that I will actually be using. vs the word_list which is all of the words in the text file.
 # count is the number of words that python will collect for each matching letter words.
+
+# it seems that x never reaches three words.
+
+
 def get_n_overlap(password, n):
     overlapping_words = []
+    x = 0
     for word in word_list:
-        overlap = set(password) & set(word)
-        if len(overlap) == n and word != password:
-            overlapping_words.append(word)
+        if x < 3:
+            overlap = set(password) & set(word)
+            if len(overlap) == n and word != password:
+                overlapping_words.append(word)
+                x += 1
+                if x == 3:
+                    break
     return overlapping_words
 
-game_words = {}
-password = "reverse"
-
-# implemented a for loop to target the dictionary so that 
-for i in range(5):
-    game_words[i] = get_n_overlap(password, i)
-
-game_words[0] = get_n_overlap(password, 0)
-game_words[1] = get_n_overlap(password, 1)
-game_words[2] = get_n_overlap(password, 2)
-game_words[3] = get_n_overlap(password, 3)
-game_words[4] = get_n_overlap(password, 4)
-
+def display_board(): 
+    pass
 
 # DRIVER CODE ---------------------
+# these lines of code are here to prevent not defined issues.
+password = get_password(word_list)
+word_list = get_word_list()
+game_words_dictionary = {}
+game_words = []
+# we can target keys in a dictionary with variables!
+# I want to use this technique once I get all three of them in there.
 
+game_words_dictionary[0] = get_n_overlap(password, 0)
+game_words_dictionary[1] = get_n_overlap(password, 1)
+game_words_dictionary[2] = get_n_overlap(password, 2)
+game_words_dictionary[3] = get_n_overlap(password, 3)
+game_words_dictionary[4] = get_n_overlap(password, 4)
+
+# print("This is the password.", password)
+# print("This is game_words", game_words_dictionary)
+
+game_word_set = sum(game_words_dictionary.values(), [])
+game_word_set.append(password)
+random.shuffle(game_word_set)
+print("This is the final game word set", game_word_set)
 
 def main():
     introduce()
+    get_word_list()
+    get_password(word_list)
+    
+
+    pass
 
 
 # If this program was run (instead of imported), run the game:
